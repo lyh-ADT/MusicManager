9d85417e2b875932d67713d9f6f576388d2373bd
lyh-ADT <1043970238@qq.com>
2020-07-10 14:53:19 +0800

    添加 建表语句

---
+++ etc/create-table.sql
@@ -0,0 +1,25 @@
+create table song(
+    sid int primary key auto_increment,
+    url varchar(500) not null unique,
+    sname varchar(255) not null
+);
+
+create table user(
+    uid int primary key auto_increment,
+    pwd varchar(255) not null,
+    cookies varchar(255)
+);
+
+create table music_list(
+    mlid int primary key auto_increment,
+    uid int not null,
+    mlname varchar(255) not null,
+    constraint FK_music_list_uid foreign key (uid) references user(uid)
+);
+
+create table music_list_detail(
+    sid int not null,
+    mlid int not null
+    constraint FK_music_list_detail_sid foreign key (sid) references song(sid),
+    constraint FK_music_list_detail_mlid foreign key (smlidid) references music_list(mlid)
+);
