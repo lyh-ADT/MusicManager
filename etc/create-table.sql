0a09369231239746037a3fcaef269a14262c5ee7
lyh-ADT <1043970238@qq.com>
2020-07-11 17:49:30 +0800

    根据dsq的要求修改数据库表结构

--- etc/create-table.sql
+++ etc/create-table.sql
@@ -1,27 +1,24 @@
 create table song(
     sid int primary key auto_increment,
     url varchar(500) not null unique,
-    sname varchar(255) not null
-
-    -- ass picture
-
+    sname varchar(255) not null,
+    imgUrl varchar(255)
 );
 
 create table user(
     uid int primary key auto_increment,
     pwd varchar(255) not null,
-    cookies varchar(255)
-
-     -- add level
+    cookies varchar(255),
+    level varchar(30)
 );
 
 create table music_list(
     mlid int primary key auto_increment,
     uid int not null,
     mlname varchar(255) not null,
+    imgUrl varchar(255),
+    description varchar(500),
     constraint FK_music_list_uid foreign key (uid) references user(uid)
-    -- add picture
-    -- add descrption
 );
 
 create table music_list_detail(
