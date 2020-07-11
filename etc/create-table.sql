9a2443337fb9e7b51167d15dccc7da6ca547a2d1
DSQ <1518656040@qq.com>
2020-07-11 15:26:13 +0800

    需要添加的属性

--- etc/create-table.sql
+++ etc/create-table.sql
@@ -2,12 +2,17 @@ create table song(
     sid int primary key auto_increment,
     url varchar(500) not null unique,
     sname varchar(255) not null
+
+    -- ass picture
+
 );
 
 create table user(
     uid int primary key auto_increment,
     pwd varchar(255) not null,
     cookies varchar(255)
+
+     -- add level
 );
 
 create table music_list(
@@ -15,11 +20,13 @@ create table music_list(
     uid int not null,
     mlname varchar(255) not null,
     constraint FK_music_list_uid foreign key (uid) references user(uid)
+    -- add picture
+    -- add descrption
 );
 
 create table music_list_detail(
     sid int not null,
-    mlid int not null
+    mlid int not null,
     constraint FK_music_list_detail_sid foreign key (sid) references song(sid),
     constraint FK_music_list_detail_mlid foreign key (smlidid) references music_list(mlid)
 );
