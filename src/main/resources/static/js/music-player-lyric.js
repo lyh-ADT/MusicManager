94ec4f9d1e6831b6033a58d6f1e766d2d0d54ca3
lyh_ADT <1043970238@qq.com>
2020-07-24 21:35:43 +0800

    添加歌曲详情Lyrics类绑定MusicPlayer播放时间更新监听器 添加Lyrics切换高亮歌词逻辑

--- src/main/resources/static/js/music-player-lyric.js
+++ src/main/resources/static/js/music-player-lyric.js
@@ -1,10 +1,26 @@
 class Lyrics{
+    static highlightCssClass = "active";
+
+    /**
+     * 把00:00:00格式的字符串转换成秒钟数
+     * @param {string} text 
+     * @return {number} 秒钟数
+     */
+    static parseTime(text) {
+        const components = text.split(":");
+        const hours = parseInt(components[0]);
+        const minutes = parseInt(components[1]);
+        const seconds = parseInt(components[2]);
+        return ((hours * 60) + minutes) * 60 + seconds;
+    }
+
     constructor(text, music_player){
         this.current_line = 0;
         this.root = document.getElementById("lyrics");
         this.script = this.parseText(text);
         this.fillLyric();
         this.music_player = music_player;
+        music_player.bindTimeUpdateListener(this.updateHightlight);
     }
 
     parseText(text){
@@ -12,7 +28,8 @@ class Lyrics{
         const list = text.split("\n");
         for(const line of list){
             const arr = line.split("]", 2);
-            const time = arr[0];
+            // 去掉前面的 "["
+            const time = arr[0].substring(1);
             const content = arr[1];
             script.push({
                 time: time,
@@ -30,5 +47,35 @@ class Lyrics{
             p.dataset["timestamp"] = line.time;
             this.root.appendChild(p);
         }
+    }
+
+    updateHighlight = () => {
+        const currentTime = this.music_player.currentTime;
+        let index = this.script.length-1;
+        for (let i = 0; i < this.script.length; ++i) {
+            const line = this.script[i];
+            const line_timestamp = Lyrics.parseTime(line.time);
+            if (line_timestamp > currentTime) {
+                index = i;
+                break;
+            }
+        }
+        this.setHighlightByIndex(index);
+    }
+
+    setHighlightByIndex(index) {
+        const lines = this.root.querySelectorAll("p");
+        if (lines.length <= index) {
+            throw new Error("下标越界啦");
+        }
+        const timestamp = this.script[index].time;
+        let needUpdate = null;
+        for (let line of lines) {
+            line.classList.remove(Lyrics.highlightCssClass);
+            if (line.dataset.timestamp == timestamp) {
+                needUpdate = line;
+            }
+        }
+        needUpdate.classList.add(Lyrics.highlightCssClass);
     }
 }
