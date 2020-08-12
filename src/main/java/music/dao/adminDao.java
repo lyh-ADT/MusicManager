0e3c9473fac05812bfbcd5ba2ca46d44d68ce1b3
recharbao <884520248@qq.com>
2020-08-12 17:42:51 +0800

    登陆注册

--- src/main/java/music/dao/adminDao.java
+++ src/main/java/music/dao/adminDao.java
@@ -1,6 +1,7 @@
 package music.dao;
 
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
@@ -8,13 +9,17 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Map;
 
+
 @Repository
 @Mapper
 public interface adminDao {
-    @Select("select * from adminInfo where account = #{account} and pwd = #{pwd}")
-    Map<String,String> login(@Param("account") String account, @Param("pwd") String pwd);
+    @Select("select * from user where nickname = #{nickname} and pwd = #{pwd}")
+    Map<String,String> login(@Param("nickname") String nickname, @Param("pwd") String pwd);
 
 
-    @Select("insert into adminInfo values (0,#{account},#{pwd})")
-    int addAdmin(@Param("account") String account, @Param("pwd") String pwd);
+    @Insert("insert into user values (0,#{pwd},null,#{nickname},'884520248@qq.com')")
+    int addAdmin(@Param("nickname") String nickname, @Param("pwd") String pwd);
 }
+
+
+
