da82c04c28fa371f383c16a332c02f672958d4ad
DSQ <1518656040@qq.com>
2020-08-13 10:10:31 +0800

    修正adminController.java文件“

--- src/main/java/music/controller/admincontroller.java
+++ src/main/java/music/controller/admincontroller.java
@@ -3,10 +3,12 @@ package music.controller;
 import music.dao.adminDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.Map;
 
 @Controller
@@ -18,14 +20,36 @@ public class admincontroller {
     @PostMapping("/regist")
     @ResponseBody
     public int regist(String account,String pwd){
-         admindao.addAdmin(account,pwd);
-         return 1;
+        admindao.addAdmin(account,pwd);
+        return 1;
     }
+
+    @PostMapping("/usermessage")
+    @ResponseBody
+    public String usermessage(HttpSession session){
+        String nickname = (String)session.getAttribute("nickname");
+        return nickname;
+    }
+
+
+
 
     @PostMapping("/login")
     @ResponseBody
-    public int login(String account, String pwd){
+    public int login(HttpServletRequest request){
+        String account = request.getParameter("account");
+        String pwd = request.getParameter("pwd");
+
         Map<String,String> map = admindao.login(account,pwd);
+
+        if (map == null){
+            return -1;
+        }
+
+        HttpSession session = request.getSession();
+        session.setAttribute("nickname",map.get("nickname"));
+        session.setAttribute("pwd",map.get("pwd"));
+        session.setAttribute("uid", map.get("uid"));
         return 1;
     }
 }
